f6406331c20d2700301f125542995962d25eb889
Adam Wagh <adamwagh@gmail.com>
2022-07-04 18:07:01 -0500

    another save

--- assets/javascript/style.js
+++ assets/javascript/style.js
@@ -9,13 +9,14 @@ console.log(currentTime)
 console.log(scheduleTime[0])
 let result = scheduleTime[0].slice(0,1);
 
-// if scheduleTime > currentTime {
-//     .addclass('future');
-// }else if scheduleTime == currentTime {
-//     .addclass('present')
-// }else {
-//     .addclass('future')
-// }
 
 $(".hour").each(function () {
-    console.log(this)})
+    console.log(this)
+})
+    // if scheduleTime > currentTime {
+    //     .addclass('future');
+    // }else if scheduleTime == currentTime {
+    //     .addclass('present')
+    // }else {
+    //     .addclass('future')
+    
