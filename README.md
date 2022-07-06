aeace116b72c6a2abd250d2260e6513831ad8b28
Adam Wagh <adamwagh@gmail.com>
2022-07-06 09:27:01 -0500

    Final commit, everything working properly

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # work-day-scheduler
 
+## Project Description
+This is a coding challenge for unit 5 of the UT Austin Coding Bootcamp. The goal of this project was to make a work day scheduler that stored input to local storage and dynamically changed colors based on the time of day 
+![alt text](./assets/images/screenshot.jpg)
+
+## How to Use This Project
+1) Go to  https://adamwagh512.github.io/work-day-scheduler/
+2) Enter your appointments on the appropriate timebar
+3) click the blue save button on the right side of the page to save the appointment
+4) refresh the page to ensure that the data was saved
+5) Enjoy a more structured daily schedule
+
+## Credits
 Couldn't find exact save icon used in demo, so I used the check2 icon found here: https://icons.getbootstrap.com/icons/check2/
+
+Special Thanks to TAs Ian and Diem for all of their invaluable help during this project. I would also like to thank my tutor Rukshan Uddin for helping me better understand some of the code and giving me some resources that really helped me finish this project. 
+
+## License
+MIT License
+
+Copyright (c) 2022 adamwagh512
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
--- assets/javascript/style.js
+++ assets/javascript/style.js
@@ -1,22 +1,96 @@
-var today = moment();
+//If we have no savedText, set it to an empty array
+if (!savedText){
+    var savedText = [];
+}
+//between the hours of 8 and 17 execute this function
+for (let index = 8; index < 18; index++) {
+    if (localStorage.getItem(`${index}`) != null){
+        savedText[index - 8] = JSON.parse(localStorage.getItem(`${index}`))
+    } else {
+        savedText[index - 8] = ""
+    }
+    console.log(`Number ${index} ${savedText[index - 8]}`)
+}
+
+// var savedText = {}
+// when document is ready, fire off the following function.
+$(document).ready(function (){
+
+// })
+//finish var today format (look at class activities )
+var today = moment().format('MMMM Do YYYY'); 
 var scheduleTime = $('.hour');
 
-$("#currentDay").text(today.format('MMMM Do YYYY'))
 
-var currentTime = moment().hour()
-console.log(currentTime)
+// $("#currentDay").text(today.format('MMMM Do YYYY'))
+$("#currentDay").append(today)
 
-console.log(scheduleTime[0])
-let result = scheduleTime[0].slice(0,1);
+//sets the current time by hour
+var currentTime = moment().hour();
+//checking my work on the currentTime variable
+console.log(currentTime);
 
-
-$(".hour").each(function () {
-    console.log(this)
+//check the value of each block hour
+$('.time-block').each(function () {
+        var blockHour = ($(this).attr
+        ('value'));
+   //logs the block hours to make sure its functioning properly     
+        console.log(blockHour);
+    //conditional set of instruction to see if the time block is past, present or future
+        //if current time < block hour the timeblock should be green
+        if (currentTime < blockHour) {
+            $(this).attr("style", "background: #77dd77");
+        // if the current time is the same as the block hour the block should be red
+        } else if (currentTime == blockHour) {
+            $(this).attr("style", "background: #ff6961");
+        //If neither of these other two conditions are met, it must be in the past, and should be gray
+        } else {
+            $(this).attr("style", "background: #d3d3d3");
+        }
+    })
+}
+)
+//When a saved button is clicked, the following function will execute
+$('.saveBtn').on('click',function () {
+// console.log($(this).prev().val())
+//grabs the attribute of data-time and assigns it to the timeBlock var
+var timeBlock = this.getAttribute('data-time')
+// we have an array of saved text, and the timeblock
+console.log(`timeBlock : ${timeBlock}`)
+console.log(`savedText : ${savedText}`)
+savedText= $(this).prev().val()
+console.log(savedText)
+//grabs the saved text and saves it to local storage. it has to be a string to be saved to local storage so we stringify it here.
+localStorage.setItem(`${timeBlock}`,JSON.stringify(savedText))
 })
-    // if scheduleTime > currentTime {
-    //     .addclass('future');
-    // }else if scheduleTime == currentTime {
-    //     .addclass('present')
-    // }else {
-    //     .addclass('future')
-    
+
+//Im sure I could make this a loop somehow, but this was the way I could get the code to work. I grabbed the ID hidden in each textarea and assigned it to the coresponding text string. Seems to work well!
+var al = document.getElementById('1a')
+al.textContent = `${savedText[0]}`
+
+var bl = document.getElementById('1b')
+bl.textContent = `${savedText[1]}`
+
+var al = document.getElementById('1c')
+al.textContent = `${savedText[2]}`
+
+var al = document.getElementById('1d')
+al.textContent = `${savedText[3]}`
+
+var al = document.getElementById('1e')
+al.textContent = `${savedText[4]}`
+
+var al = document.getElementById('1f')
+al.textContent = `${savedText[5]}`
+
+var al = document.getElementById('1g')
+al.textContent = `${savedText[6]}`
+
+var al = document.getElementById('1h')
+al.textContent = `${savedText[7]}`
+
+var al = document.getElementById('1i')
+al.textContent = `${savedText[8]}`
+
+var al = document.getElementById('1j')
+al.textContent = `${savedText[9]}`
